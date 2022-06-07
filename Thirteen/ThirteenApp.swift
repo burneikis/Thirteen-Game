3963a19871e3d8dc41c9c19b2dbb5ef484c7e5ff
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-06-07 12:32:37 +1000

    Initial Commit

---
+++ Thirteen/ThirteenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirteenApp.swift
+//  Thirteen
+//
+//  Created by Alexander Burneikis on 7/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct ThirteenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
